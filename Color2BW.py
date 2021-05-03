04c5d18cc47d76e1cf4b4639a3e790aee9036eed
Won-Doo Seo <wdseo@yonsei.ac.kr>
2021-05-03 15:52:41 +0900

    Update

--- Color2BW.py
+++ Color2BW.py
@@ -1,7 +1,7 @@
 import os
 import cv2
 
-aug_path_dir = "C:/Users/mirac/Documents/Pycharm/VAE/Aug_Dataset_Thorax/"
+aug_path_dir = "C:/Users/mirac/Documents/Pycharm/VAE/Dataset_Thorax/"
 bw_path_dir = "C:/Users/mirac/Documents/Pycharm/VAE/BW_Dataset_Thorax/"
 file_list = os.listdir(aug_path_dir)
 data_num = len(file_list)
@@ -14,4 +14,4 @@ for img in file_list:
     dat_gray = cv2.cvtColor(dat, cv2.COLOR_BGR2GRAY)
     cv2.imwrite(bw_path_dir + "BW_" + img, dat_gray)
     count += 1
-    print(str(count) + " / " + str(data_num) + " Stack Finished ...")
+    print(str(count) + " / " + str(data_num) + " Change Finished ...")
