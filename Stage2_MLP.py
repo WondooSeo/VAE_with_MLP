66a22ac1acbca16e027a29c809d272bff1eefc49
Won-Doo Seo <62936579+WondooSeo@users.noreply.github.com>
2022-02-16 19:01:48 +0900

    Update Stage2_MLP.py

--- Stage2_MLP.py
+++ Stage2_MLP.py
@@ -6,8 +6,8 @@ from PIL import Image
 from sklearn.model_selection import train_test_split
 
 
-bw_dataset_thorax_path_dir = "C:/Users/mirac/Documents/Pycharm/VAE/EIT_FER/"
-vdiff_path_dir = "C:/Users/mirac/Documents/Pycharm/VAE/EIT_V/"
+bw_dataset_thorax_path_dir = "Write your dir"
+vdiff_path_dir = "Write your dir"
 bw_dataset_thorax_file_list = os.listdir(bw_dataset_thorax_path_dir)
 vdiff_file_list = os.listdir(vdiff_path_dir)
 bw_dataset_thorax_data_num = len(vdiff_file_list)
@@ -46,7 +46,7 @@ else:
 # Normalize pixel value & expand dims to fit the input of encoder
 bw_dataset_thorax_stacking = np.expand_dims(bw_dataset_thorax_stacking, -1)
 
-encoder_path = "C:/Users/mirac/Documents/Pycharm/VAE/" + 'encoder_EIT_FER.h5'
+encoder_path = "Write your dir" + 'encoder.h5'
 if (os.path.exists(encoder_path)):
     encoder = tf.keras.models.load_model(encoder_path, compile=False)
     # encoder.summary()
@@ -72,20 +72,6 @@ x_train, x_test, y_train, y_test = train_test_split(vdiff_stacking, encoder_resu
 print("Data split Finished ...")
 
 
-# # NO USE
-# # ## Making latent vector layer code ##
-# # loc_z_mean = len(encoder.layers) - 11
-# # loc_z_log_var = len(encoder.layers) - 10
-# # z_mean = encoder.layers[loc_z_mean]
-# # z_log_var = encoder.layers[loc_z_log_var]
-# # z_mean_weights = z_mean.get_weights()[0]
-# # z_mean_bias = z_mean.get_weights()[1]
-# # z_log_var_weights = z_log_var.get_weights()[0]
-# # z_log_var_bias = z_log_var.get_weights()[1]
-# # z_weights = z_mean_weights + np.exp(0.5 * z_log_var_weights)
-# # z_bias = z_mean_bias + np.exp(0.5 * z_log_var_bias)
-
-
 def create_model():
     MLP_model = tf.keras.Sequential(name='MLP_Model')
     MLP_model.add(tf.keras.layers.Dense(256, activation="relu", input_shape=(208,)))
@@ -108,5 +94,5 @@ test_scores = stage2_model.evaluate(x_test, y_test, verbose=0)
 print("Test Loss : ", test_scores[0])
 print("Test Accuracy : ", test_scores[1])
 
-stage2_model_path = "C:/Users/mirac/Documents/Pycharm/VAE/" + "MLP_EIT_FER.h5"
+stage2_model_path = "Write your dir" + "MLP.h5"
 stage2_model.save(stage2_model_path)
